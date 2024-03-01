3059c06861aa967472ad1a430ea5ecf73462e69b
Anurag Singh <131255604+Untolders@users.noreply.github.com>
2024-03-01 21:44:59 +0530

    Create Peak-element-Solution.cpp

---
+++ Peak-element-Solution.cpp
@@ -0,0 +1,118 @@
+###################################################################################  Question  ###################################################################################################################
+
+
+
+Given an 0-indexed array of integers arr[] of size n, find its peak element. An element is considered to be peak if it's value is greater than or equal to the values of its adjacent elements (if they exist).
+
+Note: The output will be 1 if the index returned by your function is correct; otherwise, it will be 0.
+
+Example 1:
+
+Input: 
+n = 3
+arr[] = {1, 2, 3}
+Peak element's index: 2
+Output: 
+1
+Explanation: 
+If the index returned is 2, then the output printed will be 1. Since arr[2] = 3 is greater than its adjacent elements, and there is no element after it, we can consider it as a peak element. No other index satisfies the same property.
+Example 2:
+
+Input:
+n = 7
+arr[] = {1, 1, 1, 2, 1, 1, 1}
+Output: 
+1
+Explanation: 
+In this case there are 5 peak elements with indices as {0,1,3,5,6}. Returning any of them will give you correct answer.
+Your Task:
+You don't have to read input or print anything. Complete the function peakElement() which takes the array arr[] and its size n as input parameters and return the index of the peak element.
+
+Expected Time Complexity: O( log(n) ).
+Expected Auxiliary Space: O(1)
+
+Constraints:
+1 ≤ n ≤ 105
+1 ≤ arr[i] ≤ 106
+
+
+
+
+
+
+
+
+####################################################################################  Solution  ##################################################################################################################
+
+
+
+  //{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+/* The function should return the index of any
+   peak element present in the array */
+
+// arr: input array
+// n: size of array
+class Solution
+{
+    public:
+    int peakElement(int arr[], int n)
+    {
+       // Your code here
+        if(arr[0] >= arr[1]) return 0;
+       else if(arr[n - 1] >= arr[n - 2]) return n - 1;
+       else{
+           for(int i = 0; i < n; i++){
+               if(arr[i] >= arr[i + 1] && arr[i] >= arr[i-1]) return i;
+               else if(arr[i] > arr[i+1]) i++;
+           }
+       }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		int a[n], tmp[n];
+		for(int i=0;i<n;i++)
+		{
+			cin>>a[i];
+			tmp[i] = a[i];
+		}
+		bool f=0;
+		Solution ob;
+		
+		int A = ob. peakElement(tmp,n);
+		
+		if(A<0 and A>=n)
+		    cout<<0<<endl;
+		else
+		{
+    		if(n==1 and A==0)
+    		    f=1;
+    		else if(A==0 and a[0]>=a[1])
+    		    f=1;
+    		else if(A==n-1 and a[n-1]>=a[n-2])
+    		    f=1;
+    		else if(a[A]>=a[A+1] and a[A]>= a[A-1])
+    		    f=1;
+    		else
+    		    f=0;
+    		cout<<f<<endl;
+		}
+		
+	}
+
+	return 0;
+}
+// } Driver Code Ends
